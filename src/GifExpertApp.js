72a5563e4c02ec5e7836c78c2ea7e2e5dbbc30d1
Marlon Veliz <marlonveliz.nero19@gmail.com>
2021-06-13 12:56:51 -0600

    Commit desde VS code

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -11,6 +11,7 @@ const GifExpertApp = () => {
             <h2>GifExpertApp</h2>
             <AddCategory setCategories={ setCategories } />
             <hr />
+
             <ol>
                 {
                     categories.map( category => 
